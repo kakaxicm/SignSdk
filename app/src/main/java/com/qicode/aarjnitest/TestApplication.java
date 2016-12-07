ff0bf6737fa5a3c6248578fe7db888e989636ebe
chenming <583431043@qq.com>
2016-12-07 17:31:38 +0800

    发布正式的aar

--- app/src/main/java/com/qicode/aarjnitest/TestApplication.java
+++ app/src/main/java/com/qicode/aarjnitest/TestApplication.java
@@ -13,7 +13,7 @@ public class TestApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        QiCodeApi.init(this, "key_c219bbde461c49c52f847e19149d784d", "secret_a6a05183fc1e92f0a5fdb13f0a14cef5");//test
-        QiCodeApi.init(this, "key_2048b3aba40d686a1805fbc52bf971b2", "secret_99dd5cc6b374d2873a8bbf7333575973");
+        QiCodeApi.init(this, "key_c219bbde461c49c52f847e19149d784d", "secret_a6a05183fc1e92f0a5fdb13f0a14cef5");//test
+//        QiCodeApi.init(this, "key_2048b3aba40d686a1805fbc52bf971b2", "secret_99dd5cc6b374d2873a8bbf7333575973");
     }
 }

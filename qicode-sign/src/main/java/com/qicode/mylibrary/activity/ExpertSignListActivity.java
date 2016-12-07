f2c9bf6f663ffe2f38f2d1a9ec71740bfe7c11b6
chenming <583431043@qq.com>
2016-12-07 20:20:02 +0800

    首页标题UI微调

--- qicode-sign/src/main/java/com/qicode/mylibrary/activity/ExpertSignListActivity.java
+++ qicode-sign/src/main/java/com/qicode/mylibrary/activity/ExpertSignListActivity.java
@@ -3,10 +3,12 @@ package com.qicode.mylibrary.activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 import android.widget.TextView;
 
 import com.qicode.mylibrary.R;
 import com.qicode.mylibrary.fragment.ExpertSignListFragment;
+import com.qicode.mylibrary.util.SizeUtils;
 
 /**
  * Created by chenming on 16/11/24.
@@ -27,6 +29,9 @@ public class ExpertSignListActivity extends FragmentActivity {
 
         TextView titleTv = (TextView) findViewById(R.id.tv_left_title);
         titleTv.setText(R.string.expert_sign);
+
+        findViewById(R.id.iv_left).setVisibility(View.GONE);
+        titleTv.setPadding((int) SizeUtils.dp2Px(this, 10), 0, 0, 0);
     }
 
     private int setLayoutViewId() {
